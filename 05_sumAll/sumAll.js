759322246bdc921fc7799f5c2fe666e61c7948df
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-17 13:37:08 -0300

    Add excercises 1 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let minNum, maxNum;
+    let sum = 0;
 
+    
+    
+    if(num1<0 || num2<0 || (num1%1)!==0 || (num2%1)!==0 || typeof(num1)!=="number" || typeof(num2)!=="number") {
+        sum = "ERROR";
+    } else if(num1 >= num2){
+        maxNum = num1;
+        minNum = num2;
+    } else {
+        minNum = num1;
+        maxNum = num2;
+    }
+
+    for(let i = minNum; i <= maxNum; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  let temperatureInCelsius = Math.round(((5/9) * (temperatureInFahrenheit-32)) * 10) / 10;
+  return temperatureInCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  let temperatureInFahrenheit = Math.round(((9/5) * temperatureInCelsius + 32) * 10) / 10;
+  return temperatureInFahrenheit;
 };
 
 // Do not edit below this line
